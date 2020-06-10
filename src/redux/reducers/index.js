4fa2b1926620f1159d9031cea582063f46903dd6
svw <stephguirand@aol.com>
2020-06-10 13:25:53 -0400

    fixed reducersfile

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,15 @@
 import { combineReducers } from "redux";
 import { authReducer } from "./auth";
 import { usersReducer } from "./users";
-
-export default combineReducers({ auth: authReducer, users: usersReducer });
 import { messagesReducer } from "./messages";
 
 export default combineReducers({
   auth: authReducer,
+  users: usersReducer,
   messages: messagesReducer,
 });
+
+// export default combineReducers({
+//   auth: authReducer,
+//   messages: messagesReducer,
+// });
