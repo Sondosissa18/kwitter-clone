d9a487c7108c5b7ce873c4b4e88260775f734323
Sondos <sondosissa18@gmail.com>
2020-06-12 15:00:21 -0500

    added css

--- src/components/message-feed/MessageFeed.js
+++ src/components/message-feed/MessageFeed.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { Loader } from "../loader";
 import "./MessageFeed.css";
-import { PostMessageContainer } from "../post-message/"
-import { AddLikeContainer } from "../add-like"
-import { Container, Row, Col } from "react-bootstrap";
+import { PostMessageContainer } from "../post-message/";
+import { AddLikeContainer } from "../add-like";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 export class MessageFeed extends Component {
   constructor(props) {
@@ -18,21 +18,23 @@ export class MessageFeed extends Component {
     this.onListOfMessages();
   }
 
-  
-
   render() {
     return (
       <Container style={{ marginTop: "5%" }}>
         <div id="messageFeed">
           <div id="messages">
             <PostMessageContainer />
+            <Card style={{ width: "18rem" }}></Card>
             {this.props.messages.length > 0 &&
               this.props.messages.map((message) => (
                 <div key={message.id}>
                   <p>{message.username}</p>
                   <p>{message.text}</p>
                   <p>Likes: {message.likes.length}</p>
-                  <AddLikeContainer messageIdProps={message.id} likes={message.likes}/>
+                  <AddLikeContainer
+                    messageIdProps={message.id}
+                    likes={message.likes}
+                  />
                 </div>
               ))}
 
