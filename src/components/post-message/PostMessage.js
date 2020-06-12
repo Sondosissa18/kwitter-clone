d0cfd8aa7f426ff23ccd4f5b472663e93bbf0b90
Sondos <sondosissa18@gmail.com>
2020-06-11 22:29:09 -0500

    added css to messages page

--- src/components/post-message/PostMessage.js
+++ src/components/post-message/PostMessage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./PostMessage.css";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Container } from "react-bootstrap";
 
 export const PostMessage = ({ createMessage, listOfMessages, error }) => {
   // Not to be confused with "this.setState" in classes
@@ -23,29 +23,32 @@ export const PostMessage = ({ createMessage, listOfMessages, error }) => {
   return (
     <React.Fragment>
       <br />
-      <br />
-      <Card border="primary" style={{ width: "18rem" }}>
-        <Card.Header>Add New Message</Card.Header>
-        <Card.Body>
-          <Card.Title>Message</Card.Title>
-          <Card.Text>
-            <form id="message-form" onSubmit={submitMessage}>
-              <input
-                type="text"
-                name="text"
-                value={state.text}
-                autoFocus
-                required
-                onChange={handleChange}
-                placeholder=" Type a new message"
-              />
-              <br />
-              <button variant="primary">Post</button>
-            </form>
-          </Card.Text>
-          <Button variant="primary">Post</Button>
-        </Card.Body>
-      </Card>
+      {/* <br /> */}
+      <Container>
+        <Card border="info" style={{ marginBottom: "20px", width: "35%" }}>
+          <Card.Header>Add New Message</Card.Header>
+          <Card.Body>
+            <Card.Title>Message</Card.Title>
+            <hr />
+            <Card.Text>
+              <form id="message-form" onSubmit={submitMessage}>
+                <input
+                  type="text"
+                  name="text"
+                  value={state.text}
+                  autoFocus
+                  required
+                  onChange={handleChange}
+                  placeholder=" Type a new message"
+                />
+                <br />
+              </form>
+            </Card.Text>
+            <button variant="primary">Post</button>
+            {/* <Button variant="primary">Post</Button> */}
+          </Card.Body>
+        </Card>
+      </Container>
 
       {error && <p style={{ color: "red" }}>{error.message}</p>}
       {console.log(state)}
